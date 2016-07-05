0da35f3108f1f76df4353ba0554c238984914aae
sujin2_25 <sujin2_25@281a6b47-5e35-0510-aecb-c9a4f9c3992f>
2016-07-05 11:48:20 +0000

    ckeditor/ckfinder

---
+++ mommefatale/src/main/webapp/resources/ckeditor/config.js
@@ -0,0 +1,15 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	 config.filebrowserBrowseUrl ='/mommefatale/resources/ckfinder/ckfinder.html';
+	 config.filebrowserImageBrowseUrl ='/mommefatale/resources/ckfinder/ckfinder.html?type=Images';
+	 config.filebrowserUploadUrl ='/mommefatale/ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Files';
+	 config.filebrowserImageUploadUrl ='/mommefatale/ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Images';
+};
