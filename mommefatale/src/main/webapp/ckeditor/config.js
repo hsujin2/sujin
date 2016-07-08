094db815e6b272921e0fa938f400c1ca0f8d320d
sujin2_25 <sujin2_25@281a6b47-5e35-0510-aecb-c9a4f9c3992f>
2016-07-08 07:54:36 +0000

    ckEditor/ ckFinder 경로 이동

--- mommefatale/src/main/webapp/ckeditor/config.js
+++ mommefatale/src/main/webapp/ckeditor/config.js
@@ -8,8 +8,8 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	 config.filebrowserBrowseUrl ='/mommefatale/resources/ckfinder/ckfinder.html';
-	 config.filebrowserImageBrowseUrl ='/mommefatale/resources/ckfinder/ckfinder.html?type=Images';
+	 config.filebrowserBrowseUrl ='/mommefatale/ckfinder/ckfinder.html';
+	 config.filebrowserImageBrowseUrl ='/mommefatale/ckfinder/ckfinder.html?type=Images';
 	 config.filebrowserUploadUrl ='/mommefatale/ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Files';
 	 config.filebrowserImageUploadUrl ='/mommefatale/ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Images';
 };
