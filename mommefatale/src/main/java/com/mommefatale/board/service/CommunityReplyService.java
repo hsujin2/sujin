4eae2c053aa4ec878dc6fb4f92ad4075fe5619a9
sujin2_25 <sujin2_25@281a6b47-5e35-0510-aecb-c9a4f9c3992f>
2016-07-17 02:57:15 +0000

    board 삭제

--- mommefatale/src/main/java/com/mommefatale/board/service/CommunityReplyService.java
+++
@@ -1,8 +0,0 @@
-package com.mommefatale.board.service;
-
-import java.util.Map;
-
-public interface CommunityReplyService {
-	
-	public void communityReply(Map<String, Object> vo);
-}
--- mommefatale/src/main/java/com/mommefatale/board/service/CommunityWriteService.java
+++
@@ -1,8 +0,0 @@
-package com.mommefatale.board.service;
-
-import java.util.Map;
-
-public interface CommunityWriteService {
-	public void communityWrite(Map<String, Object> vo);
-
-}
